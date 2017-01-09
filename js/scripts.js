dd550b1c4b3bee21d3d4329786d3270c454c600a
Lawrence Eby and Matt Caswell <lawrence@>
2017-01-09 11:20:26 -0800

    Add other functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 var add = function(number1, number2){
-	return (number1) + number2;
+	return number1 + number2;
+};
+
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
+
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+
+var bmi = function(number1, number2) {
+  var weight = number1 * 0.45;
+  var height = (number2 * 0.025);
+  var heightconvert = height * height;
+  return weight / heightconvert;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter a second number:"));
 
-alert(add(number1, number2));
+var result = bmi(number1, number2);
+alert(result);
