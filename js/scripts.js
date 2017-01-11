8f51359559df172095bcfbb1546cf0c5f6c432a5
Lawrence Eby and Nicole Sanders <lawrence@>
2017-01-11 10:16:04 -0800

    add a form for the user to enter 2 numbers and sets up jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Calculator functions
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -14,39 +15,42 @@ var divide = function(number1, number2){
 	return number1 / number2;
 };
 
-var bmi = function(number1, number2) {
-  var weight = number1 * 0.45;
-  var height = (number2 * 0.025);
-  var heightconvert = height * height;
-  return weight / heightconvert;
-};
+$(document).ready(function(){
+	$("#add form").submit(function(event){
 
-var temp = function(number1) {
-  return number1 * 1.8 + 32
-}
+		event.preventDefault();
+	});
 
-var gallonsToLiters = function (number1) {
-  return number1 * 3.785411784;
-}
+});
 
-var gallonsToQuarts = function (number1) {
-  return number1 * 4;
-}
 
-var quartsToPint = function (number1) {
-  return number1 * 2;
-}
-
-var pintToCup = function (number1) {
-  return number1 * 1.97157;
-}
-
-var cupToFluidOz = function (number1) {
-  return number1 * 8.11537;
-}
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter a second number:"));
-
-var result = gallonsToLiters(number1, number2);
-alert(result);
+// var bmi = function(number1, number2) {
+//   var weight = number1 * 0.45;
+//   var height = (number2 * 0.025);
+//   var heightconvert = height * height;
+//   return weight / heightconvert;
+// };
+//
+// var temp = function(number1) {
+//   return number1 * 1.8 + 32
+// }
+//
+// var gallonsToLiters = function (number1) {
+//   return number1 * 3.785411784;
+// }
+//
+// var gallonsToQuarts = function (number1) {
+//   return number1 * 4;
+// }
+//
+// var quartsToPint = function (number1) {
+//   return number1 * 2;
+// }
+//
+// var pintToCup = function (number1) {
+//   return number1 * 1.97157;
+// }
+//
+// var cupToFluidOz = function (number1) {
+//   return number1 * 8.11537;
+// }
