909b123d4fffa0201d746db4c8d1ac6d6bd87096
Katy Daviscourt and Nicole Sanders <katy@>
2017-01-12 14:37:23 -0800

    add the javascript and jquery to perform math depending on the type of operation that was chosen

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,41 @@
 //Calculator functions
 var add = function(number1, number2){
-	return number1 + number2;
+	var result = number1 + number2;
+	return result;
 };
 
 var subtract = function(number1, number2){
-	return number1 - number2;
+	var result = number1 - number2;
+	return result;
 };
 
 var multiply = function(number1, number2){
-	return number1 * number2;
+	var result = number1 * number2;
+	return result;
 };
 
 var divide = function(number1, number2){
-	return number1 / number2;
+	var result = number1 / number2;
+	return result;
 };
 
 //User Interface
 $(document).ready(function(){
-	$("#add form").submit(function(event){
+	$("form#math").submit(function(event){
 		var number1Input = parseFloat($("input#number1").val());
 		var number2Input = parseFloat($("input#number2").val());
 
-		var result = add(number1Input, number2Input);
-		$(".output").text(result);
-		event.preventDefault();
-	});
+		if ($("input:radio[name=operator]:checked").val() === "add"){
+			result = add(number1Input,number2Input);
+		} else if ($("input:radio[name=operator]:checked").val() === "subtract"){
+			result = subtract(number1Input,number2Input)
+		} else if ($("input:radio[name=operator]:checked").val() === "multiply"){
+			result = multiply(number1Input,number2Input)
+		} else if ($("input:radio[name=operator]:checked").val() === "divide"){
+			result = divide(number1Input,number2Input)
+		}
 
-	$("#subtract form").submit(function(event){
-		var number1Input = parseFloat($("input#subnumber1").val());
-		var number2Input = parseFloat($("input#subnumber2").val());
 
-		var result = subtract(number1Input, number2Input);
-		$(".output").text(result);
-		event.preventDefault();
-	});
-
-	$("#multiply form").submit(function(event){
-		var number1Input = parseFloat($("input#mulnumber1").val());
-		var number2Input = parseFloat($("input#mulnumber2").val());
-
-		var result = multiply(number1Input, number2Input);
-		$(".output").text(result);
-		event.preventDefault();
-	});
-
-	$("#divide form").submit(function(event){
-		var number1Input = parseFloat($("input#divnumber1").val());
-		var number2Input = parseFloat($("input#divnumber2").val());
-
-		var result = divide(number1Input, number2Input);
 		$(".output").text(result);
 		event.preventDefault();
 	});
